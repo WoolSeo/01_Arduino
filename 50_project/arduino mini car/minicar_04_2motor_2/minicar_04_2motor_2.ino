6d09fcd8356e9cf7e0ff76fb42e87bab31d5523c
Wool Seo <wool@wool.pe.kr>
2017-07-23 17:38:05 +0900

    update

--- 50_project/arduino mini car/minicar_04_2motor_2/minicar_04_2motor_2.ino
+++ 50_project/arduino mini car/minicar_04_2motor_2/minicar_04_2motor_2.ino
@@ -1,9 +1,9 @@
-const int L_motor = 10;
-const int R_motor = 11;
+const int L_motor = 9;
+const int R_motor = 10;
 
 void setup() {
-  analogWrite(L_motor, 230);
-  analogWrite(R_motor, 230);
+  analogWrite(L_motor, 200);
+  analogWrite(R_motor, 200);
   delay(1000);
 
   analogWrite(L_motor, 0);
