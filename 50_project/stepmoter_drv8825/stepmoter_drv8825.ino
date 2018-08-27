8e36f4aec49b57f489dc895d0220d4858b7ea8cf
Wool Seo <wool@wool.pe.kr>
2018-08-27 23:13:06 +0900

    drv8825

---
+++ 50_project/stepmoter_drv8825/stepmoter_drv8825.ino
@@ -0,0 +1,20 @@
+int steps=10;
+int dir =11;
+int stepTime=100;
+
+
+
+void setup() {
+  
+  pinMode(steps, OUTPUT);
+  pinMode(dir, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(dir, HIGH);
+  digitalWrite(steps, HIGH);   
+  delay(stepTime);                      
+  digitalWrite(steps, LOW);    
+  delay(stepTime);                       
+}
